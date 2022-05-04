bcac0a390f47b0eb9375b7bde9879da1036c137d
Dimitry Zub <dimitryzub@gmail.com>
2022-05-04 09:14:47 +0300

    app: add basic layout MiCard app

--- mi_card_app/lib/main.dart
+++ mi_card_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'middle_info.dart';
+import 'contact_info.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, // removes debug label
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        body: MiddleInfo(),
       ),
     );
   }
---
+++ mi_card_app/lib/middle_info.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class MiddleInfo extends StatelessWidget {
+  const MiddleInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+              'https://images.unsplash.com/photo-1638742385167-96fc60e12f59?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80'),
+          fit: BoxFit.cover,
+          alignment: Alignment.center, // could be used for animating background
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const CircleAvatar(
+              radius: 60.0,
+              backgroundImage: NetworkImage(
+                  'https://images.pexels.com/users/avatars/18651040/dimitry-zub-845.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2'),
+            ),
+            const Text(
+              'Dmitriy Zub',
+              style: TextStyle(
+                fontSize: 55,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const Text(
+              'Developer Advocate',
+              style: TextStyle(
+                fontSize: 23,
+                fontFamily: 'Mono',
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+              child: const Text(
+                'learning flutter/dart',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Mono',
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
